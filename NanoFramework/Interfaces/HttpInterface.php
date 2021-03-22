97de20270bc2754480c4921ce5419ec62a5c8833
georgii <george.webfullstack@gmail.com>
2021-03-22 23:04:10 +0300

    Add Http interface

--- NanoFramework/Interfaces/HttpInterface.php
+++ NanoFramework/Interfaces/HttpInterface.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace NanoFramework\Interfaces;
 
+use SplFixedArray;
+
 /**
  * Interface HttpInterface
  *
@@ -10,6 +12,18 @@ namespace NanoFramework\Interfaces;
  */
 interface HttpInterface
 {
+    /**
+     * Get multiple content by urls.
+     *
+     * @param array|string $urls
+     * @return SplFixedArray|null
+     */
+    public static function get(array|string $urls): ?SplFixedArray;
 
-
+    /**
+     * @param string $url
+     * @param array  $info
+     * @return bool|string
+     */
+    public static function post(string $url, array $info): bool|string;
 }
